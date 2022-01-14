b81a051f7729bec16f84e431c6c6732f29993b80
AdrianRodriguez20 <59927052+AdrianRodriguez20@users.noreply.github.com>
2022-01-14 13:18:35 +0000

    Instituto Swagger -> Ordenar carpetas

---
+++ Tareas de Clase/TC Documentar Api con Swagger/Matriculas/src/test/java/es/iespuertodelacruz/juan/matriculas/MatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.juan.matriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
