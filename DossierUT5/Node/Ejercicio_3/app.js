9530d24abe47af95be45f73045393fac451775dc
AdrianRodriguez20 <adrirodriguezfuentes@gmail.com>
2022-01-19 19:56:55 +0000

    Actividades Node -> Numeros primos , funciones flecha

--- DossierUT5/Node/Ejercicio_3/app.js
+++ DossierUT5/Node/Ejercicio_3/app.js
@@ -1,7 +1,7 @@
-function getTabla(tabla) {
+const getTabla = (tabla) => {
     let limite = 10;
     let respuesta = "";
-    for (let i = 1; i < limite; i++) {
+    for (let i = 1; i <= limite; i++) {
         respuesta += `${tabla} * ${i} = ${tabla * i} \n`;
 
     }
--- DossierUT5/Node/Ejercicio_4/app.js
+++ DossierUT5/Node/Ejercicio_4/app.js
@@ -1,5 +1,6 @@
 const { Console } = require('console');
 const fs = require('fs');
+const { boolean } = require('yargs');
 
 function getTabla(tabla) {
     let limite = 10;
@@ -14,14 +15,58 @@ function getTabla(tabla) {
     }
     return respuesta;
 }
-//writeFile() es asíncrono. Así que hay un callback
-fs.writeFile(
-    'tabla.txt', //nombre del fichero
-    getTabla(4), //información a guardar
-    (err) => { //callback respuesta al finalizar
-        if (err)
-            console.log("No se pudo grabar el archivo");
-        else
-            console.log("se ha grabado");
+
+
+
+const grabarFichero = (nombre , datos ) =>{
+
+    fs.writeFile(
+        nombre, //nombre del fichero
+         datos, //información a guardar
+        (err) => { //callback respuesta al finalizar
+            if (err)
+                console.log("No se pudo grabar el archivo");
+            else
+                console.log("se ha grabado");
+        }
+    );
+    
+}
+
+grabarFichero("tabla.txt", getTabla(5));
+
+// ------------------------------------------------------------------//
+//                          NÚMEROS PRIMOS
+// ------------------------------------------------------------------//
+
+const getPrimos = () => {
+    let array =[];
+  
+    for (let i = 0; i <= 200; i++) {
+        if (esPrimo(i)) {
+            array[i]=i;
+          
+        };
     }
-);
+    return array;
+}
+
+const esPrimo = (numero) =>
+{
+    let esPrimoActual = true;
+    if (numero < 2) {
+        esPrimoActual = false;
+    } else {
+        for (let x = 2; x * x <= numero; x++) {
+            if (numero % x == 0) {
+                esPrimoActual = false;
+                break;
+            }
+        }
+    }
+    return esPrimoActual;
+
+}
+grabarFichero("primos.txt", getPrimos().join(' ').replace(/\s+/g, ' '))
+
+
